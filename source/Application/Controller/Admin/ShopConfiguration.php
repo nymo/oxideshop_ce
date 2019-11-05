8918b483d8d97ec1bc3d1c6a8651dff9c8a2e481
Abbas Afsharfarnia <afshar5152@gmail.com>
2019-11-05 12:53:34 +0100

    OXDEV-555 add option for having other shops news in multishops

--- source/Application/Controller/Admin/ShopConfiguration.php
+++ source/Application/Controller/Admin/ShopConfiguration.php
@@ -153,6 +153,11 @@ class ShopConfiguration extends \OxidEsales\Eshop\Application\Controller\Admin\A
                             continue;
                         }
                         $this->saveSetting($configName, $existingConfigType, $newConfigValue);
+
+                        if ($configName == 'blUnloadAllShopNews') {
+                            $shopMall = oxNew(\OxidEsales\Eshop\Application\Controller\Admin\ShopMall::class);
+                            $shopMall->changeNewsInheritanceForMultishop(!filter_var($newConfigValue, FILTER_VALIDATE_BOOLEAN));
+                        }
                     }
                 }
             }
